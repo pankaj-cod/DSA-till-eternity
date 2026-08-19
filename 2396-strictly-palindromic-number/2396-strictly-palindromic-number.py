f7d9a854a15c15d4445afe7a31f1de39020c60ce
pankaj-cod <upadhyaypankaj322@gmail.com>
2026-08-19 13:45:31 +0530

    Time: 0 ms (100%), Space: 12.3 MB (66.35%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isStrictlyPalindromic(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        # for i in range(2,n-1):
+        #     string = baseconvert.base(n,10,i,string=True)
+        #     if string!=string[::-1]:
+        #         return False
+        
+        # return True
+        return False
