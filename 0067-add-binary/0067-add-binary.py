e830847192a0abaced2bd0b0c7422903cf281025
pankaj-cod <upadhyaypankaj322@gmail.com>
2026-09-02 14:06:01 +0530

    Time: 0 ms (100%), Space: 12.4 MB (22.55%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -5,21 +5,5 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
-        i = len(a)-1
-        j = len(b)-1
-        carry = 0
-        ans = []
-
-        while i>=0 or j>=0 or carry:
-
-            digit_a = int(a[i]) if i>=0 else 0
-            digit_b = int(b[j]) if j>=0 else 0
-
-            total = digit_a+digit_b+carry
-            carry = total//2
-            
-            ans.append(str(total%2))
-            i-=1
-            j-=1
-        
-        return "".join(ans[::-1])
+        Sum = int(a,2)+int(b,2)
+        return bin(Sum)[2:]
